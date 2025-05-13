e567c599012770ec0adeb0eee245c0dd25cd0bc9
Seth Tierney <seth@nub.im>
2025-05-13 17:46:02 -0400

    remove file deletion function (for now)

--- js/LoadImageGallery.js
+++ js/LoadImageGallery.js
@@ -24,20 +24,7 @@ const ext = {
                 font-size: 0!important;
                 position: relative;
             }
-			.delete-button {
-                position: absolute;
-                top: 2px;
-                right: 2px;
-                width: 20px;
-                height: 20px;
-                background-color: rgba(255, 0, 0, 0.7);
-                color: white;
-                border-radius: 50%;
-                display: flex;
-                justify-content: center;
-                cursor: pointer;
-                font-size: 14px !important;
-            }
+
 			.tab {
 			  padding: 5px 10px;
 			  margin-right: 5px;
@@ -75,27 +62,7 @@ const ext = {
 			request.onerror = event => console.error("Error reading thumbnails from DB: " + event.target.error);
 			FirstRun = false;
 		};
-        async function deleteFile(filename) {
-            try {
-                const response = await fetch('/delete_file', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ filename }),
-                });
-                if (response.ok) {
-                    console.log(`File ${filename} deleted successfully`);
-                    return true;
-                } else {
-                    console.error(`Failed to delete file ${filename}`);
-                    return false;
-                }
-            } catch (error) {
-                console.error('Error deleting file:', error);
-                return false;
-            }
-        }
+
 		async function removeThumbnail(filename) {
 			return new Promise((resolve, reject) => {
 				const transaction = db.transaction(['thumbnails'], 'readwrite');
@@ -367,21 +334,8 @@ const ext = {
 
 						entry.style.backgroundImage = `url('${thumbnailUrl}')`;
 						
-						const deleteButton = document.createElement('div');
-						deleteButton.classList.add('delete-button');
-						deleteButton.textContent = '×';
-						deleteButton.setAttribute('title', 'Delete');
-						deleteButton.addEventListener('click', async (e) => {
-							e.stopPropagation();
-							if (await deleteFile(filename)) {
-								entry.remove();
-								await removeThumbnail(filename);
-								
-								values.splice(index, 1);
-							}
-						});
-						entry.appendChild(deleteButton);
 					});
+					
 			}			
 				
 				});
